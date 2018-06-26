7d46d5d17c6c0210b3b676c3791ca3138c52891a
Jesus Martinez <jesusda91@gmail.com>
2018-06-26 17:43:49 -0500

    add power select, change document type input for power select, add javascript validations in subscribe form

--- app/components/footer-app/subscribe-form/component.js
+++ app/components/footer-app/subscribe-form/component.js
@@ -1,9 +1,46 @@
+/* global swal */
 import Component from '@ember/component';
-
+import { match } from '@ember/object/computed';
 export default Component.extend({
+
+	documentTypes: Object.freeze(["CEDULA DE CIUDADANIA", "PASAPORTE", "CEDULA EXTRANJERA", "NIT", "TARJETA DE IDENTIDAD"]),
+
+	isEmailValid: match('email', /^([a-z0-9_.-]+)@([\da-z.-]+)\.([a-z.]{2,6})$/),
+
 	actions: {
-		login() {
-			console.log("onSubscribe");
+		onSubscribe() {
+			const { name, email, document, phone, documentType, allRights } = this;
+			const isEmailValid = this.get("isEmailValid");
+			if (!isEmailValid) {
+				return swal(
+					'Error!',
+					'El correo electronico ingresado no es valido',
+					'error'
+				)
+			}
+			if (name && email && document && phone && documentType && allRights) {
+				this.blankFields();
+				return swal(
+					'Exito!',
+					'Te has suscrito satisfactoriamente!',
+					'success'
+				)
+			}
+			return swal(
+				'Error!',
+				'Todos los campos son requeridos!',
+				'error'
+			);
+
 		}
+	},
+
+	blankFields() {
+		this.set("name", null);
+		this.set("email", null);
+		this.set("document", null);
+		this.set("phone", null);
+		this.set("documentType", null);
+		this.set("allRights", null);
 	}
 });
